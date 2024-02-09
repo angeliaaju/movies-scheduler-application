e59bc642ee710282c0392844c5f7524b23095c8b
angeliaaju <159000566+angeliaaju@users.noreply.github.com>
2024-02-10 03:51:46 +0530

    Update MovieRepository.java

--- src/main/java/com/movie/aggregator/repository/MovieRepository.java
+++ src/main/java/com/movie/aggregator/repository/MovieRepository.java
@@ -3,7 +3,6 @@ package com.movie.aggregator.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.movie.aggregator.domain.Movie;
 import com.movie.aggregator.domain.Movies;
 
 /**
